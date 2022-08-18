ce6db1bc65e563d6612a48f2350c74e9f30f499e
HyunsuChoi-git <choihs1054@gmail.com>
2022-08-18 17:24:38 +0900

    study promise2

--- promise.js
+++ promise.js
@@ -14,6 +14,67 @@ const promise = new Promise((resorve, reject) => {
      // 네트워크 통신을 통해 데이터를 가져오거나 파일을 읽는 등의 doing some heavy work
      console.log('doing something');
      setTimeout(()=>{
-        resorve('- 응답완료 -');
+        resorve('- 응답완료 -'); 
+        //reject(new Error('- 오류발생 -'));
      }, 2000);
 });
+
+
+// 2. consumers, then, catch, finally
+
+promise.then((res) => { //then : promise 수행이 정상적으로 완료되면 콜백함수에서 resorve로 전달된 데이터가 파라미터로 들어옴.
+   console.log(res);
+})
+.catch((err)=>{   // catch : 수행 오류가 있을 경우에 들어옴
+   console.log(err);
+})
+.finally(()=>{ // finally : 무조건 마지막에 실행되는 함수
+   console.log('- 끝 -');
+})
+
+
+// 3. promise chaining
+const fetchNum = new Promise((res, rej) => {
+   setTimeout(()=> res(1), 1000);
+});
+
+fetchNum
+   .then((num) => num+2)
+   .then((num) => num*3)
+   .then((num) => {  // then 내부에 또다른 Promise를 호출하여 resorve를 리턴할 수 있다!.
+      return new Promise((res, rej)=>{
+         setTimeout(()=>{ res(num-1); },1000);
+      })
+   })
+   .then((num)=>{
+      console.log(num);
+   });
+
+
+// 4. promise chaining Exception처리
+const getChicken = new Promise((res, rej) => {
+      setTimeout(() => { res('🐔'); }, 1000);
+   });
+const getEgg = chicken =>
+   new Promise((res, rej) => {
+      setTimeout(() => { 
+         rej(new Error(`${chicken} =X 🥛`));
+         res(`${chicken} => `+'🥚'); 
+      }, 1000);
+   });
+
+const cookFry = egg =>
+   new Promise((res, rej) => {
+      setTimeout(() => { res(`${egg} => `+'🍳'); }, 1000);
+   });
+   
+//getChicken
+//   .then( chicken => getEgg(chicken) )
+//   .then( egg => cookFry(egg) )
+//   .then( meal => console.log(meal) );
+ 
+getChicken
+   .then(getEgg)
+   .then(cookFry)
+   .then(console.log)
+   .catch(console.log);
